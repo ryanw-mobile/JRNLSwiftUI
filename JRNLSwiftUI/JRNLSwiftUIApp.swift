26a06d68662c87316af2eacc0f4a0616efe5763a
Ryan Wong <ryan@rwmobi.com>
2024-04-02 21:40:05 +0100

    Initial Commit

---
+++ JRNLSwiftUI/JRNLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNLSwiftUIApp.swift
+//  JRNLSwiftUI
+//
+//  Created by 🇭🇰Ry Wong on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct JRNLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
