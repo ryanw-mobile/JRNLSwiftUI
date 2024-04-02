3de980b73553861c36b9a0a36009a0771c4f4e6d
Ryan Wong <ryan@rwmobi.com>
2024-04-02 22:24:50 +0100

    [feature] Journal List View: loc 8019 implement list view with navigation

--- JRNLSwiftUI/ContentView.swift
+++ JRNLSwiftUI/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    var journalEntries: [JournalEntry] = testData
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(self.journalEntries) { journalEntry in
+                JournalCell(journalEntry: journalEntry)
+            }.navigationTitle("Journal List")
+                .navigationDestination(for: JournalEntry.self) {
+                    JournalEntry in
+                    Text(JournalEntry.entryTitle)
+                }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct JournalCell: View {
+    var journalEntry: JournalEntry
+    var body: some View {
+        NavigationLink(value: self.journalEntry) {
+            VStack {
+                HStack {
+                    Image(uiImage: self.journalEntry.photo ?? UIImage(systemName: "face.smiling")!)
+                        .resizable()
+                        .frame(width: 90, height: 90)
+                    VStack {
+                        Text(self.journalEntry.date.formatted(.dateTime.day().month().year()))
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(self.journalEntry.entryTitle)
+                            .font(.title2)
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    }
+                }
+            }
+        }
+    }
 }
